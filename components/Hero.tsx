88d82e0facf9c02b43df7b53b5d8f384592ac925
meltjacTechs <ebwire02@gmail.com>
2024-05-20 12:17:15 +0300

    chore: Update npm dependencies, add new packages, and improve Hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -40,7 +40,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi!👋 I&apos;m Manuel, a Next.js Developer based in Croatia.
+            Hi!👋 I&apos;m Manuel, a Next.js Developer based in Kenya.
           </p>
 
           <a href="#about">
