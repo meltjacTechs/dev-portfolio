fecf17aebf35d5841864079290af19180ffc0ed1
meltjacTechs <ebwire02@gmail.com>
2024-05-23 19:50:00 +0300

    refactor: Update RecentProjects component to use <p> instead of <a> for live site link

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -66,9 +66,9 @@ const RecentProjects = () => {
                 </div>
 
                 <div className="flex justify-center items-center">
-                  <a className="flex lg:text-xl md:text-xs text-sm text-purple">
+                  <p className="flex lg:text-xl md:text-xs text-sm text-purple">
                     Check Live Site
-                  </a>
+                  </p>
                   <FaLocationArrow className="ms-3" color="#CBACF9" />
                 </div>
               </div>
